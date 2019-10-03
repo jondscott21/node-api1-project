9a6bcc396fb9f9cbd3703e48917cdde3869a07b6
Jonathan Scott <jonathan.scott@gmail.com>
2019-10-03 15:25:20 -0700

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+const server = express();
+
+server.use(express.json())
+
+const port = 5000;
+
+server.get('/', (req, res) => {
+    res.send('Hello world from express')
+})
+
+server.get('/users', (req, res) => {
+    db.find().then(users => {
+        res.send(users)
+    })
+    .catch(err => res.status(500).json({ error: "The users information could not be retrieved." }))
+})
+
+server.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id).then(user => {
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+        res.send(user)
+    })
+    .catch(err => res.json({ message: "error finding the user" }))
+})
+
+server.post('/users', (req, res) => {
+    const newUser = req.body;
+    db.insert(newUser).then(user => {
+        if(!user.name || !user.bio) {
+            res.status(500).json({ errorMessage: "Please provide name and bio for the user." })
+        }
+        res.status(201).json(user)
+    })
+    .catch(err => res.json({ errorMessage: "Error creating the user." }))
+})
+
+server.put('/user/:id', (req, res) => {
+    const newUser = req.body;
+    const id = req.params.id;
+    db.update(id, newUser).then(user => {
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist."})
+        }
+        res.json(user)
+    })
+    .catch(err => res.json({ message: "Error updating the user."}))
+})
+
+server.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id).then(user => {
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist."})
+        }
+        res.json(user)
+    })
+    .catch(err => res.json({ message: "Error deleting the user."}))
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
